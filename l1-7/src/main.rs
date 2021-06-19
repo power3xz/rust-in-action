7fa9b37d937424db1cf95a29d1c883904b6ad041
devj <power3xz@gmail.com>
2021-06-19 13:31:02 +0900

    add l1-7 compile result

--- l1-7/src/main.rs
+++ l1-7/src/main.rs
@@ -4,3 +4,6 @@ fn main() {
 
     assert_eq!(buffer_overflow, '🍉')
 }
+
+// thread 'main' panicked at 'index out of bounds: the len is 3 but the index is 4', l1-7/src/main.rs:3:27
+// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
