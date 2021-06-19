43138e30b19884af547899636b0a8363986e5bdd
devj <power3xz@gmail.com>
2021-06-19 13:29:42 +0900

    add l1-5 compile result

--- l1-5/src/main.rs
+++ l1-5/src/main.rs
@@ -16,3 +16,39 @@ fn main() {
 
     println!("{:?}", grains)
 }
+// error[E0382]: borrow of moved value: `grains`
+//   --> l1-5/src/main.rs:17:22
+//    |
+// 12 |     let mut grains: Vec<Cereal> = vec![];
+//    |         ---------- move occurs because `grains` has type `Vec<Cereal>`, which does not implement the `Copy` trait
+// ...
+// 15 |     drop(grains);
+//    |          ------ value moved here
+// 16 |
+// 17 |     println!("{:?}", grains)
+//    |                      ^^^^^^ value borrowed here after move
+
+// error[E0382]: borrow of moved value: `grains`
+//   --> l1-5/src/main.rs:17:22
+//    |
+// 12 |     let mut grains: Vec<Cereal> = vec![];
+//    |         ---------- move occurs because `grains` has type `Vec<Cereal>`, which does not implement the `Copy` trait
+// ...
+// 15 |     drop(grains);
+//    |          ------ value moved here
+// 16 |
+// 17 |     println!("{:?}", grains)
+//    |                      ^^^^^^ value borrowed here after move
+
+// error: aborting due to previous error
+
+// For more information about this error, try `rustc --explain E0382`.
+// error: aborting due to previous error
+
+// For more information about this error, try `rustc --explain E0382`.
+// error: could not compile `l1-5`
+
+// To learn more, run the command again with --verbose.
+// error: could not compile `l1-5`
+
+// To learn more, run the command again with --verbose.
