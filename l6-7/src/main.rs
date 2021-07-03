409f37b437ad161cb1dc57f9410ca9123f97dcf6
devj <power3xz@gmail.com>
2021-07-04 01:31:59 +0900

    fix 6.7

--- l6-7/src/main.rs
+++ l6-7/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let a_ptr = &a as *const i64;
     let a_addr: usize = unsafe { std::mem::transmute(a_ptr) };
 
-    println!("a: {}, ({:p}...0x{:x}", a, a_ptr, a_addr + 7);
+    println!("a: {}, ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
 }
 
-// a: 42, (0x7ffee57fc0d8...0x7ffee57fc0df
+// a: 42, (0x7ffee2ce70d8...0x7ffee2ce70df)
